85c439c35ee4c7a7f9a688332a58074d102f3abd
wangchuanli001 <wangchuanli_@hotmail.com>
2019-06-14 15:31:47 +0800

    shouye xinzeng div

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,6 @@
 
     //网站最后更新时间（版本更新需更改）
-    var siteLastUpdateTime = '2019年05月23日13点';
+    var siteLastUpdateTime = '2019年06月02日13点';
 
     //网站开始时间
     var siteBeginRunningTime = '2018-07-25 20:00:00';
