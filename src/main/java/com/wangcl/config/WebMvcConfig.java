3c7aff01533453fb6c7b0d53c21416d42782d4d2
wangchuanli001 <wangchuanli_@hotmail.com>
2019-06-15 15:53:06 +0800

    ERROR错误界面

--- src/main/java/com/wangcl/config/WebMvcConfig.java
+++ src/main/java/com/wangcl/config/WebMvcConfig.java
@@ -31,12 +31,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
         return new EmbeddedServletContainerCustomizer() {
             @Override
             public void customize(ConfigurableEmbeddedServletContainer container) {
-                ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401");
-                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+                ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/404");
+                ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/404");
                 ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/403");
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+                ErrorPage error408Page = new ErrorPage(HttpStatus.REQUEST_TIMEOUT, "/404");
                 ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500");
+                ErrorPage error502Page = new ErrorPage(HttpStatus.BAD_GATEWAY,"/404");
 
-                container.addErrorPages(error401Page,error404Page,error403Page,error500Page);
+                container.addErrorPages(error400Page,error401Page,error404Page,error403Page,error408Page,error500Page,error502Page);
             }
         };
     }
--- src/main/java/com/wangcl/controller/ErrorPageControl.java
+++ src/main/java/com/wangcl/controller/ErrorPageControl.java
@@ -12,13 +12,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ErrorPageControl {
 
     @GetMapping("/404")
-    public String error404(){
+    public String error404() {
         return "errorpage/404";
     }
 
     @GetMapping("/403")
-    public String error403(){
+    public String error403() {
         return "errorpage/403";
+    }
+
+    @GetMapping("/401")
+    public String error401() {
+        return "errorpage/401";
+    }
+
+    @GetMapping("/500")
+    public String error500() {
+        return "errorpage/500";
     }
 
 
