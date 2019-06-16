3cfcfc8c40cdeaef55eeb01fdd229cacf15261bc
wangchuanli001 <wangchuanli_@hotmail.com>
2019-06-16 11:09:43 +0800

    ERROR错误界面

--- src/main/java/com/wangcl/controller/ErrorPageControl.java
+++ src/main/java/com/wangcl/controller/ErrorPageControl.java
@@ -11,14 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorPageControl {
 
-    @GetMapping("/404")
-    public String error404() {
-        return "errorpage/404";
-    }
-
-    @GetMapping("/403")
-    public String error403() {
-        return "errorpage/403";
+    @GetMapping("/400")
+    public String error400() {
+        return "errorpage/400";
     }
 
     @GetMapping("/401")
@@ -26,10 +21,29 @@ public class ErrorPageControl {
         return "errorpage/401";
     }
 
+    @GetMapping("/403")
+    public String error403() {
+        return "errorpage/403";
+    }
+
+    @GetMapping("/404")
+    public String error404() {
+        return "errorpage/404";
+    }
+
+    @GetMapping("/408")
+    public String error408() {
+        return "errorpage/408";
+    }
+
     @GetMapping("/500")
     public String error500() {
         return "errorpage/500";
     }
 
+    @GetMapping("/502")
+    public String error502() {
+        return "errorpage/502";
+    }
 
 }
